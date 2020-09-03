08fd9db94468e37928d55180b62f8e4553060a90
IRONKAGE <IRONKAGE@gmail.com>
2020-09-03 14:55:17 +0300

    write Logic

---
+++ Assets/Scripts/Idle.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Idle : MonoBehaviour
+{
+    [Header ("Stats")]
+    public float speed;
+    public float nearDistance;
+
+    [Header ("References")]
+    private Transform player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("agressionNPC").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector2.Distance(transform.position, player.position) < nearDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
+        }
+        else if(Vector2.Distance(transform.position, player.position) >= nearDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        }
+    }
+
+    void OnBecameVisible()
+    {
+        return;
+    }
+}
---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Move : MonoBehaviour
+{
+    [Header ("Parameters")]
+    public float speed;
+    public float time;
+    public int newAim;
+    public NavMeshAgent nav;
+    public Vector3 pos;
+    public Vector3 aim;
+
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        time += Time.deltaTime;
+        if(time >= newAim)
+        {
+            NewAim();
+            time = 0;
+        }
+    }
+
+    void NewAim()
+    {
+        Vector3 pos = gameObject.transform.position;
+
+        aim = new Vector3(pos.x + Random.Range(-speed, speed), pos.y , pos.z + Random.Range( -speed, speed));
+
+        nav.SetDestination(aim);
+        newAim = Random.Range(1, 5);
+    }
+}
